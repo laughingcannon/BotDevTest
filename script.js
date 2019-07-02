3eed1aaa42e25eaeaadf3bb42a0193b14178fc92
Abhinav Kannan <abhinavkannan@gmail.com>
2019-07-02 10:27:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,74 +15,56 @@
 
 // console.log(data);
 
-// data.forEach(movie => 
-// {
-//   // Log each movie's title
-//   console.log(movie.title);
-// })
+var words = []; // Array of words from txt file.
+var wordsMap = {}; // To create map of words and their frequency.
+var api_key = 'dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf'; // API key to call Yandex.
 
-// }
-
-// Send request
-// request.send();
-
-// Promise.all([
-//   fetch('big.txt').then(x => x.text())
-// ]).then(([sampleResp]) => {
-// 	console.log(sampleResp[1000]);
-//   console.log(sampleResp);
-// });
-
-var words = [];
-var wordsMap = {};
-var api_key = 'dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf';
-
-fetch('big.txt')
+fetch('big.txt') // Currently fetching directly from txt, and saving the stream in a variable "text". 
   .then(response => response.text())
   .then((text) => {
-// words = text.split(" ");
-words = text.split(/\s+/);
-// console.log(words);
+  
+  // words = text.split(" ");
+  words = text.split(/\s+/); // Removing spaces and gathering words in array.
+  // console.log(words);
 
-// for (var i = 0; i < words.length; i++) {
-//   var num = words[i];
-//   counts[num] = counts[num] ? counts[num] + 1 : 1;
-// }
+  // Now creating a map of words and their frequency.
 
   words.forEach(function (key)
   {
     if (wordsMap.hasOwnProperty(key))
     {
-      wordsMap[key]++;
+      wordsMap[key]++; // Increment count of a word if it already exists.
     }
     else
     {
-      wordsMap[key] = 1;
+      wordsMap[key] = 1; // Assign a count of 1.
     }
   });
 
 // console.log(wordsMap);
 
-  var finalWordsArray = [];
+  var finalWordsArray = []; // Array of objects.
   finalWordsArray = Object.keys(wordsMap).map(function (key) {
-    return {
+    return
+    {
       name: key,
       total: wordsMap[key]
     };
   });
 
-  finalWordsArray.sort(function (a, b) {
+  finalWordsArray.sort(function (a, b) // Sort in descending order.
+  {
     return b.total - a.total;
   });
 
   // console.log(finalWordsArray);
-  for (var i = 0; i < 10; ++i)
+  for (var i = 0; i < 10; ++i) // Print top 10 frequently occuring words.
   {
-  	console.log(finalWordsArray[i].name + ": " + finalWordsArray[i].total);
+    console.log(finalWordsArray[i].name + ": " + finalWordsArray[i].total);
   }
 
-  	function getSynonymsPOS(word, num)
-  	{
+  function getSynonymsPOS(word, num) // Function to make API call to Yandex for synonyms and POS.
+  {
 	  	var xhr2 = new XMLHttpRequest();
 	  	var url_dict = 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=' + api_key + '&lang=en-en&text=' + JSON.stringify(word);
 
@@ -92,23 +74,24 @@ words = text.split(/\s+/);
 		  if (xhr2.readyState === XMLHttpRequest.DONE)
 		    {
 		      // return xhr2.response;
-		      finalWordsArray[num].result = xhr2.response;
+		      finalWordsArray[num].result = xhr2.response; // Add result to finalWordsArray object. 
 		    }
 		}
 	  	xhr2.open('GET', url_dict);
 	  	xhr2.send();
-  	}
+  }
 
 
-  	let promises = [];
-	for (i = 0; i < 10; ++i)
-	{
-		promises.push(getSynonymsPOS(finalWordsArray[i].name, i));
-	}
+  let promises = [];
+  for (i = 0; i < 10; ++i)
+  {
+	promises.push(getSynonymsPOS(finalWordsArray[i].name, i));
+  }
 
-	Promise.all(promises);
-	for (var j = 0; j < 10; ++j)
-	{
-		console.log(finalWordsArray[j]);
-	}
+  Promise.all(promises); //Execute all as promises.
+	
+  for (var j = 0; j < 10; ++j)
+  {
+	console.log(finalWordsArray[j]);
+  }
 });
